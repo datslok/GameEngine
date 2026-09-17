a84ad4f4e307d8bc3a1c4f86afcbffa4013887e4
Axel Kolstad <axelko@ulrik.uio.no>
2026-09-18 00:25:03 +0200

    added a pixelbuffer header

---
+++ src/pixelbuffer.h
@@ -0,0 +1,14 @@
+#pragma once
+#include <cstddef>
+#include <vector>
+#include "pixel.h"
+
+class PixelBuffer {
+    private:
+        std::size_t width;
+        std::size_t height;
+        std::vector<Pixel> pixels;
+
+    public:
+        PixelBuffer(std::size_t width, std::size_t height);
+};
