f4eb94b7b14ed0b1a6a4edc24c68e3b82226419b
Axel Kolstad <axelko@ulrik.uio.no>
2026-09-18 01:03:22 +0200

    added getters and setters to the buffer header

--- src/pixelbuffer.h
+++ src/pixelbuffer.h
@@ -12,4 +12,9 @@ class PixelBuffer {
     public:
         PixelBuffer(std::size_t width, std::size_t height);
         void print() const;
+        void setPixel(std::size_t x, std::size_t y, Pixel colour);
+        void clear(Pixel colour);
+        Pixel getPixel(std::size_t x, std::size_t y) const;
+        std::size_t getWidth() const;
+        std::size_t getHeight() const;
 };
