a850ac3bcc399c8eb7888dbbc97d54dc76ee697e
niamhrh <niamhhannon121@icloud.com>
2026-09-18 22:26:00 +0200

    Added comment to pixel.h

--- src/pixel.h
+++ src/pixel.h
@@ -1,6 +1,10 @@
 #include <cstdint>
 #pragma once
 
+/*
+* Represents a pixel using 8-bit RGB colour channels.
+* Each channel stores a value from 0 to 255.
+*/
 struct Pixel{
     uint8_t r;
     uint8_t g;
